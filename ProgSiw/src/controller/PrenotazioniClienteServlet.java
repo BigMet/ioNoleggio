197f9ed79d9430c8fd8b7d80bb19e2bd93d57758
p1997 <p1997@DESKTOP-TK51BIE>
2019-02-25 12:18:23 +0100

    prenotazioni non più in lista se in noleggio

--- ProgSiw/src/controller/PrenotazioniClienteServlet.java
+++ ProgSiw/src/controller/PrenotazioniClienteServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,9 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.Noleggio;
 import model.Prenotazione;
+import model.Veicolo;
 import persistence.DAOFactory;
+import persistence.dao.NoleggioDao;
 import persistence.dao.PrenotazioneDao;
+import persistence.dao.VeicoloDao;
 
 /**
  * Servlet implementation class PrenotazioniClienteServlet
@@ -27,7 +32,23 @@ public class PrenotazioniClienteServlet extends HttpServlet {
 		
 		DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.POSTGRESQL);
 	 	PrenotazioneDao prenotazioneDao = factory.getPrenotazioneDAO();
+	 	NoleggioDao noleggioDao = factory.getNoleggioDAO();
+	 	VeicoloDao veicoloDao = factory.getVeicoloDAO();
 	 	List <Prenotazione> prenotazioni = prenotazioneDao.findByCredential((int) session.getAttribute("idutente"));
+	 	List <Noleggio> noleggi = noleggioDao.findAll();
+	 	List<Veicolo> veicoli = new LinkedList<>();
+	 	for (Noleggio n : noleggi) {
+			for (Prenotazione p : prenotazioni) {
+				if(n.getPrenotazione().getIdPrenotazione()==p.getIdPrenotazione()) {
+					prenotazioni.remove(p);
+					break;
+				}
+			}
+		}
+		for (Prenotazione p : prenotazioni) {
+	 		veicoli.add(veicoloDao.findByPrimaryKey(p.getTarga()));
+		}
+	 	session.setAttribute("veicoli", veicoli);
 	 	session.setAttribute("prenotazioni", prenotazioni);
 	 	RequestDispatcher disp;
 	 	disp = req.getRequestDispatcher("prenotazioni.jsp");
